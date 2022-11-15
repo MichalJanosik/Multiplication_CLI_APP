1869b57944e80d04a5c82ac21346e98074e4af19
MichalJanosik <janosik.michal.jm@gmail.com>
2022-11-15 17:19:18 +0100

    create MultiplicationApp.java

---
+++ src/main/java/org/example/MultiplicationApp.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class MultiplicationApp {
+
+    private String[] args;
+
+    public void setArgs(String[] args) {
+        this.args = args;
+    }
+
+    public void runApp() {
+
+    }
+}
